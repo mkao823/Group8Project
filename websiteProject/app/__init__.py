d0cf03461010755f489c4be9f3951a2c3c49743a
Michael Kao <michael.kao@sjsu.edu>
2022-04-29 13:11:41 -0700

    added structure to initial files, and added models.py to start database

--- websiteProject/app/__init__.py
+++ websiteProject/app/__init__.py
@@ -1,5 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 myapp_obj = Flask(__name__)
 
-from app import routes
+myapp_obj.config.from_mapping(
+    SECRET_KEY = 'you-will-never-guess',
+    # where to store app.db (database file)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+)
+
+db = SQLAlchemy(myapp_obj)
+
+#login = LoginManager(myapp_obj)
+# function that is called to login a user
+#login.login_view = 'login'
+#commented the above lines out for now, this is in the inclassexample for the __init__ file
+
+
+
+from app import routes, models
--- websiteProject/app/routes.py
+++ websiteProject/app/routes.py
@@ -1,1 +1,15 @@
 from app import myapp_obj
+
+#should have all our routes, login, logout, create account, etc in this file
+
+@myapp_obj.route('/sign-up')
+def createAccount():
+
+@myapp_obj.route('/delete-account')
+def deleteAccount():
+
+@myapp_obj.route('/login')
+def login():
+
+@myapp_obj.route('/logout')
+def logout():
