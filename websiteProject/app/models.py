b9790ffd00a8ba807c00631ddef3073d9bfbe69d
Michael Kao <michael.kao@sjsu.edu>
2022-05-03 16:35:13 -0700

    updated create account and delete account

--- websiteProject/app/models.py
+++ websiteProject/app/models.py
@@ -7,17 +7,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     email = db.Column(db.String(64))
-    password_hash = db.Column(db.String(128))
+    password1 = db.Column(db.String(128))
 
-"""class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return f'<{self.user_id}, {self.timestamp}: {self.body}>'
-"""
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
--- websiteProject/app/routes.py
+++ websiteProject/app/routes.py
@@ -1,5 +1,7 @@
+from flask_login import current_user
 from app import myapp_obj, db
-from flask import render_template, request, flash
+from flask import render_template, request, flash, redirect, url_for
+from flask_wtf import FlaskForm
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -8,23 +10,36 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def splashPage():
     return render_template("base.html")
 
+#class SignUpForm(FlaskForm):
+
 @myapp_obj.route('/sign-up', methods=['GET', 'POST'])
 def createAccount():
     if request.method == 'POST':
-        email = request.form.get('email')
+        email = request.form.get('email') #set variables equal to the form response for 'email', same for name and password1
         name = request.form.get('name')
         password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-        flash("accout created", category = "success")
-
-        user = User(email = email, name = name, password = generate_password_hash(password1, method = 'sha256'))
-        db.session.add(user)
-        db.session.commit()
-    return render_template("sign_up.html")
+        password2 = request.form.get('password2') 
+        #flash("account created", category = "success")
+        if password1 != password2:
+            flash("Passwords must match", category = "error")
+        else:
+            user = User(email = email, name = name, password1 = generate_password_hash(password1, method = 'sha256'))
+            #creates new user with email, name, password
+            db.session.add(user) #add this created user to our database using this command, then commit 
+            db.session.commit()
+            flash('Account created!', category='success')#message on screen should notify us that account has been created, if not, this is not correct
+            #after creating account, we should make sure we login user as well
+    return render_template("sign_up.html", user = current_user)
 
 @myapp_obj.route('/delete-account', methods=['GET', 'POST'])
-def deleteAccount():
-    return "deleteacc"
+def deleteAccount(account):
+    users = User.query.all()
+    #u = User.query.get(int(id))
+    #db.session.delete(u)
+    for u in users:
+        if u == account:
+            db.session.delete(u)
+    return render_template("delete_account.html")
 
 @myapp_obj.route('/cart')
 def addToCart():
