8d33a8c8cdb1750703b4924422a4798b596136ad
Michael Kao <michael.kao@sjsu.edu>
2022-05-02 21:57:42 -0700

    Updated to create account, need to implement database creation

--- websiteProject/app/routes.py
+++ websiteProject/app/routes.py
@@ -1,5 +1,7 @@
-from app import myapp_obj
-from flask import render_template, request
+from app import myapp_obj, db
+from flask import render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 #should have all our routes, login, logout, create account, etc in this file
 @myapp_obj.route('/')
@@ -8,6 +10,16 @@ def splashPage():
 
 @myapp_obj.route('/sign-up', methods=['GET', 'POST'])
 def createAccount():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        flash("accout created", category = "success")
+
+        user = User(email = email, name = name, password = generate_password_hash(password1, method = 'sha256'))
+        db.session.add(user)
+        db.session.commit()
     return render_template("sign_up.html")
 
 @myapp_obj.route('/delete-account', methods=['GET', 'POST'])
