299c8ae7ac488ef78a469253192bfb05e5f71d49
Michael Kao <michael.kao@sjsu.edu>
2022-05-06 16:17:05 -0700

    updated run.py

--- websiteProject/run.py
+++ websiteProject/run.py
@@ -1,7 +1,4 @@
-from app import myapp_obj, createDatabase, login, User
+from app import myapp_obj, createDatabase
 createDatabase(myapp_obj)
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 myapp_obj.run()
